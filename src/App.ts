c0a5acb7df2b6d21b49b3b89033c6a1adba69d54
Evan Suma Rosenberg <suma@umn.edu>
2024-01-25 15:17:32 -0600

    class commit

--- src/App.ts
+++ src/App.ts
@@ -9,6 +9,9 @@ import * as gfx from 'gophergfx'
 export class App extends gfx.GfxApp
 {
     private ship: gfx.Mesh2;
+    private star: gfx.Mesh2;
+    
+    private starfield: gfx.Particles2;
 
     // --- Create the App class ---
     constructor()
@@ -17,6 +20,9 @@ export class App extends gfx.GfxApp
         super();
 
         this.ship = gfx.Geometry2Factory.createBox();
+        this.star = gfx.Geometry2Factory.createBox();
+
+        this.starfield = new gfx.Particles2(this.star, 200);
     }
 
 
@@ -28,6 +34,19 @@ export class App extends gfx.GfxApp
 
         this.ship.scale.set(0.08, 0.08);
         this.ship.material.texture = new gfx.Texture('./ship.png');
+        
+        this.star.material.texture = new gfx.Texture('./star.png');
+
+        for(let i = 0; i < this.starfield.numParticles; i++)
+        {
+            this.starfield.particleSizes[i] = Math.random()*0.008 + 0.002;
+            this.starfield.particlePositions[i].set(Math.random()*2-1, Math.random()*2-1);
+        }
+
+        // Update the particile system position and sizes
+        this.starfield.update(true, true);
+
+        this.scene.add(this.starfield);
         this.scene.add(this.ship);
     }
 
